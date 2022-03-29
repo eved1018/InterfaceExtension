f3bcb22b188a6072dd530f495ad9fc3d47df7dec
eved1018 <edelsteinevan@gmail.com>
2022-03-29 09:25:30 -0400

    c

--- scripts/simple_mutate.py
+++ scripts/simple_mutate.py
@@ -1,6 +1,10 @@
 import os
 import sys
 
+"""
+Adapted from https://github.com/haddocking/pdb-tools
+"""
+
 def simple_mutate(pdb, qc, respos, wt_res,mutAA):
     mutated_structure = []
     mutposition = mutAA + str(respos)
@@ -12,7 +16,6 @@ def simple_mutate(pdb, qc, respos, wt_res,mutAA):
                 s_resi = line[22:26].strip()
                 s_resn = line[17:20].strip()
                 if s_chain == qc and s_resi == respos:
-                    # check whether wildtype residue name is same as given resn.
                     if s_resn == wt_res:
                         line = line[0:17]+mutAA+line[20:]
                         mutated_structure.append(line)
@@ -28,5 +31,3 @@ def simple_mutate(pdb, qc, respos, wt_res,mutAA):
             _mutfile.write(line)
     return 
 
-# if __name__ == "__main__":
-#     mutate("input/1i85.pdb", "A", "0" ,"MET", "SER")
